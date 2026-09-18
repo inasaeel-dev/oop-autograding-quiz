2dba61659c7f553ec8a3bd31e923df6f5082c966
Inas Asami El <inasasami11@gmail.com>
2026-09-18 08:36:25 +0700

    quiz 1

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -8,8 +8,9 @@ package id.ac.polinema.oop;
  * the autograder calls them exactly as defined here.
  */
 public class Student {
-
-    // TODO: add fields: studentId (String), name (String), gpa (double)
+    private String studentId;
+    private String name;
+    private double gpa;
 
     /**
      * Student constructor.
@@ -19,23 +20,21 @@ public class Student {
      * @param gpa       Grade Point Average (0.0 - 4.0)
      */
     public Student(String studentId, String name, double gpa) {
-        // TODO: store parameters into fields
-        throw new UnsupportedOperationException("Not implemented yet");
+            this.studentId = studentId;
+            this.name = name;
+            this.gpa = gpa;
     }
 
     public String getStudentId() {
-        // TODO: return studentId
-        throw new UnsupportedOperationException("Not implemented yet");
+        return studentId;
     }
 
     public String getName() {
-        // TODO: return name
-        throw new UnsupportedOperationException("Not implemented yet");
+        return name;
     }
 
     public double getGpa() {
-        // TODO: return gpa
-        throw new UnsupportedOperationException("Not implemented yet");
+        return gpa;
     }
 
     /**
